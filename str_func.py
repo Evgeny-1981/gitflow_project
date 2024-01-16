99f14e1e985152d1b59f42a5567ffa37a3fb668d
Evgeny Egorov <eaegorov42@gmail.com>
2024-01-16 16:53:55 +0500

    create str_func.py

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def foo(stroka):
+    """Функция заменяющая все буквы слов в строке на заглавные"""
     res = stroka.upper()
+    return res
+
+def foo_2(stroka):
+    """Функция заменяющая первые буквы слов в строке на заглавные"""
+    res = stroka.title()
     return res
